ccb0abdc2e8d61f2251d2a0d782e3babf89adf6a
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2022-12-27 08:40:25 +0100

    added the contract abi and address

---
+++ constants/index.ts
@@ -0,0 +1,4 @@
+export const crytoDevGoerliAddress = "0x9d6510Ba85F8587e39b71FFE197c2093e9cBcB05";
+export const crytoDevPolygonAddress = "0x61DC32BF84A97fC259C6B5997D2efA602CCdc745";
+export const goerliIcoAddress ="0x4B8E9c3bc71868311059342AFdA1bF1105160840"
+export const polygonIcoAddress ="0xE7c8F6e301d705166ed99b748bB2A826f1Fc0FFC"
