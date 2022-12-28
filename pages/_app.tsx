2a1815ccfdfba06d0a9812dac7413b281211b092
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2022-12-28 14:37:38 +0100

    added the remaining complexities

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 
+
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div>
       <Head>
         <title>ICO Offering</title>
         <link rel="icon" href="/favicon.ico" />
+         
       </Head>
       <Component {...pageProps} />
     </div>
