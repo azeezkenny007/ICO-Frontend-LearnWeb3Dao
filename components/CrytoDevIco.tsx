30dd6fc900f59fdfedd096d0de7898312ded6893
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2022-12-28 13:32:14 +0100

    added the useEffect to monitor if the wallet has been connected

--- components/CrytoDevIco.tsx
+++ components/CrytoDevIco.tsx
@@ -253,5 +253,24 @@ export default function CrytoDevIco({}: Props) {
    }
  };
 
+
+  useEffect(() => {
+    // if wallet is not connected, create a new instance of Web3Modal and connect the MetaMask wallet
+    if (!walletConnected) {
+      // Assign the Web3Modal class to the reference object by setting it's `current` value
+      // The `current` value is persisted throughout as long as this page is open
+      web3ModalRef.current = new Web3Modal({
+        network: "goerli",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+      connectWallet();
+      getTotalTokensMinted();
+      getBalanceOfCryptoDevTokens();
+      getTokensToBeClaimed();
+      getOwner();
+    }
+  }, [walletConnected]);
+
   return <div>hello world </div>;
 }
