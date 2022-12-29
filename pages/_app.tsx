29a6cb6c3c953f73bf85d408dd40d20fa2549a03
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2022-12-29 11:41:00 +0100

    added the toast container to the _app.js

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { ToastContainer, toast } from "react-toastify";
 
 
 
@@ -12,6 +13,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="icon" href="/favicon.ico" />
          
       </Head>
+      <ToastContainer/>
       <Component {...pageProps} />
     </div>
   );
